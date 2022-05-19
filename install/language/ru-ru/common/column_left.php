d390aeeb40d8e90c2b8236d7f6c13584d91ca4d1
Michael Backend Dev <oleksiy.kizyma@yourquery.ua>
2022-05-19 04:14:37 +0300

    Theme setup

--- install/language/ru-ru/common/column_left.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Text
-$_['text_license']       = 'Лицензия';
-$_['text_installation']  = 'Подготовка к установке';
-$_['text_configuration'] = 'Настройка';
-$_['text_upgrade']       = 'Обновление';
-$_['text_finished']      = 'Завершено';
-$_['text_language']      = 'Язык';
